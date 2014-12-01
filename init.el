ffff7106a24d629ffc5582af87fc44b4a23a8f89
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-12-01 08:47:59 -0500

    only use exec path from shell with darwin

--- init.el
+++ init.el
@@ -56,16 +56,6 @@
     ;; http://www.emacswiki.org/emacs/Smex
     smex
 
-    ;; On OS X, an Emacs instance started from the graphical user
-    ;; interface will have a different environment than a shell in a
-    ;; terminal window, because OS X does not run a shell during the
-    ;; login. Obviously this will lead to unexpected results when
-    ;; calling external utilities like make from Emacs.
-    ;; This library works around this problem by copying important
-    ;; environment variables from the user's shell.
-    ;; https://github.com/purcell/exec-path-from-shell
-    exec-path-from-shell
-
     ;; project navigation
     projectile
 
@@ -77,6 +67,17 @@
 
     ;; git integration
     magit))
+
+;; On OS X, an Emacs instance started from the graphical user
+;; interface will have a different environment than a shell in a
+;; terminal window, because OS X does not run a shell during the
+;; login. Obviously this will lead to unexpected results when
+;; calling external utilities like make from Emacs.
+;; This library works around this problem by copying important
+;; environment variables from the user's shell.
+;; https://github.com/purcell/exec-path-from-shell
+(if (eq system-type 'darwin)
+    (add-to-list 'my-packages 'exec-path-from-shell))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
