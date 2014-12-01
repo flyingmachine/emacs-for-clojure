018dc06d7dafe628614c96705e38c801b82c1041
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-12-01 08:52:17 -0500

    tweaks

--- customizations/ui.el
+++ customizations/ui.el
@@ -34,8 +34,7 @@
 ;; Uncomment the lines below by removing semicolons and play with the
 ;; values in order to set the width (in characters wide) and height
 ;; (in lines high) Emacs will have whenever you start it
-;; (setq initial-frame-alist '((top . 0) (left . 0)
-;;                             (width . 177) (height . 53)))
+;; (setq initial-frame-alist '((top . 0) (left . 0) (width . 177) (height . 53)))
 
 ;; These settings relate to how emacs interacts with your operating system
 (setq ;; makes killing/yanking interact with the clipboard
