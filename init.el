4261de4b36a12b14c5ba17cf12b378c231711dff
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2015-01-22 21:09:07 -0500

    clean up redundant code

--- init.el
+++ init.el
@@ -130,10 +130,3 @@
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(coffee-tab-width 2))
