f46f86855e31a8ef1411fece854f3b7b14ca327e
treeherder <b.d.reddybest@gmail.com>
2015-01-28 01:38:24 -0800

    yas finally it works

--- init.el
+++ init.el
@@ -111,6 +111,10 @@
 ;; environment variables
 (load "shell-integration.el")
 
+;; Track package installations in Cask file automatically
+(require 'pallet)
+(pallet-mode t)
+
 ;; These customizations make it easier for you to navigate files,
 ;; switch buffers, and choose options from the minibuffer.
 (load "navigation.el")
@@ -131,3 +135,38 @@
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
+
+;; Disable annoying UI
+ (setq inhibit-splash-screen t)
+ (tool-bar-mode -1)
+ 
+ ;; UI preferences
+ (add-to-list 'default-frame-alist '(font . "Inconsolata-14"))
+ (set-face-attribute 'default t :font "Inconsolata-14")
+ (load-theme 'tangotango t)
+ 
+ ;; Backups
+ (setq backup-directory-alist '(("." . "~/.emacs.d/saves")))
+ 
+ ;; Indentation / Whitespace
+ (setq-default indent-tabs-mode nil)
+ (setq default-tab-width 4)
+ (define-key global-map (kbd "RET") 'newline-and-indent)
+ (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Insert matching pairs automatically
+ (electric-pair-mode t)
+ 
+ ;; Git
+ (require 'magit)
+ (require 'magit-gh-pulls)
+ (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
+ 
+ ;; JavaScript
+ (require 'js2-mode)
+ (autoload 'js2-mode "js2-mode" nil t)
+ (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+ 
+ ;; SASS
+ (add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+ (add-hook 'scss-mode-hook 'rainbow-mode)
