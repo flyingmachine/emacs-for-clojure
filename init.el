ab2bbfb041324b219b4628127d56e244ec5b0bfe
treeherder <b.d.reddybest@gmail.com>
2015-01-27 22:28:45 -0800

    got the split working, cask not complaining

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 ;;;;
 ;; Packages
 ;;;;
-
+(require 'cask "/usr/local/Cellar/cask/0.7.2/cask.el")
+(cask-initialize)
 ;; Define package repositories
 (require 'package)
 (add-to-list 'package-archives
@@ -130,3 +131,48 @@
 ;; Langauage-specific
 (load "setup-clojure.el")
 (load "setup-js.el")
+
+;; Track package installations in Cask file automatically
+(require 'pallet)
+(pallet-mode t)
+
+;; Disable annoying UI
+(setq inhibit-splash-screen t)
+(tool-bar-mode -1)
+
+;; UI preferences
+(add-to-list 'default-frame-alist '(font . "Inconsolata-14"))
+(set-face-attribute 'default t :font "Inconsolata-14")
+(load-theme 'tangotango t)
+
+;; Backups
+(setq backup-directory-alist '(("." . "~/.emacs.d/saves")))
+
+;; Indentation / Whitespace
+(setq-default indent-tabs-mode nil)
+(setq default-tab-width 4)
+(define-key global-map (kbd "RET") 'newline-and-indent)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Fuzzy matching
+(require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+
+;; Insert matching pairs automatically
+(electric-pair-mode t)
+
+;; Git
+(require 'magit)
+(require 'magit-gh-pulls)
+(add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
+
+;; JavaScript
+(require 'js2-mode)
+(autoload 'js2-mode "js2-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+;; SASS
+(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+(add-hook 'scss-mode-hook 'rainbow-mode)
