6b8cdca1a0fecb63d58fc7581c513869e4f5001d
南山 <junjiemars@gmail.com>
2015-07-23 23:41:52 +0800

    if the packages does not installed then install it

--- init.el
+++ init.el
@@ -3,165 +3,24 @@
 
 (add-to-list 'load-path "~/.emacs.d/config")
 
+(load "ui.el")
+
 ;; Define package repositories
 (when (and (>= emacs-major-version 24) (>= emacs-minor-version 4))
   (setq package-archives 
 	'(("gnu" . "http://elpa.gnu.org/packages/")
-	  ("melpa-stable" . "http://stable.melpa.org/packages/"))))
-  
-  (comment ("melpa" . "http://melpa.org/packages/"))
-  (comment ("marmalade" . "http://marmalade-repo.org/packages/"))
-  (comment ("melpa" . "http://melpa.org/packages"))
-
+	  ("melpa-stable" . "http://stable.melpa.org/packages/")))
   ;; Guarantee all packages are installed on start
-  (defvar packages-list
+  (defconst packages-list
     '(paredit)
     "List of packages needs to be installed at launch")
 
-  (require 'cl-lib)
-  (defun install-missing-packages ()
-    (let ((missing-packages (cl-remove-if
-			     #'package-installed-p packages-list)))
-      (when missing-packages
-	(message "Installing %d missing package(s)"
-		 (length missing-packages))
-	(package-refresh-contents)
-	(mapc #'package-install missing-packages))))
-  (install-missing-packages)
-  
-  (with-eval-after-load 'paredit
-    (warn "try to load paredit"))
+  (require 'package)
+  (package-initialize)
 
-  (message "%s" "customized load completed")
-
-  (comment
-    (require 'package)
-   (add-to-list 'package-archives
-                '("marmalade" . "http://marmalade-repo.org/packages/") t)
-   (add-to-list 'package-archives
-                '("tromey" . "http://tromey.com/elpa/") t)
-   (add-to-list 'package-archives
-                '("melpa" . "http://melpa.milkbox.net/packages/") t)
-   (add-to-list 'package-archives
-                '("org" . "http://orgmode.org/elpa/"))
-   (add-to-list 'package-archives
-                '("melpa" . "http://melpa.org/packages") t))
-  (comment
-   ;; Load and activate emacs packages. Do this first so that the
-   ;; packages are loaded before you start trying to modify them.
-   ;; This also sets the load path.
-   (package-initialize)
-
-   ;; Download the ELPA archive description if needed.
-   ;; This informs Emacs about the latest versions of all packages, and
-   ;; makes them available for download.
-   (when (not package-archive-contents)
-     (package-refresh-contents))
-
-   ;; The packages you want installed. You can also install these
-   ;; manually with M-x package-install
-   ;; Add in your own as you wish:
-   (defvar my-packages
-     '(;; makes handling lisp expressions much, much easier
-       ;; Cheatsheet: http://www.emacswiki.org/emacs/PareditCheatsheet
-       paredit
-
-       ;; key bindings and code colorization for Clojure
-       ;; https://github.com/clojure-emacs/clojure-mode
-       clojure-mode
-
-       ;; extra syntax highlighting for clojure
-       clojure-mode-extra-font-locking
-
-       ;; integration with a Clojure REPL
-       ;; https://github.com/clojure-emacs/cider
-       cider
-
-       ;; allow ido usage in as many contexts as possible. see
-       ;; customizations/navigation.el line 23 for a description
-       ;; of ido
-       ido-ubiquitous
-
-       ;; Enhances M-x to allow easier execution of commands. Provides
-       ;; a filterable list of possible commands in the minibuffer
-       ;; http://www.emacswiki.org/emacs/Smex
-       smex
-
-       ;; project navigation
-       projectile
-
-       ;; colorful parenthesis matching
-       rainbow-delimiters
-
-       ;; edit html tags like sexps
-       tagedit
-
-       ;; git integration
-       magit
-
-       ;; On OS X, an Emacs instance started from the graphical user
-       ;; interface will have a different environment than a shell in a
-       ;; terminal window, because OS X does not run a shell during the
-       ;; login. Obviously this will lead to unexpected results when
-       ;; calling external utilities like make from Emacs.
-       ;; This library works around this problem by copying important
-       ;; environment variables from the user's shell.
-       ;; https://github.com/purcell/exec-path-from-shell
-       (if (eq system-type 'darwin)
-           (add-to-list 'my-packages 'exec-path-from-shell))
-
-       (dolist (p my-packages)
-         (when (not (package-installed-p p))
-           (package-install p)))))
+  (dolist (p packages-list)
+    (when (not (package-installed-p p))
+      (message "Installing the missing %s package" p)
+      (package-install p))))
 
 
-   ;; Place downloaded elisp files in ~/.emacs.d/vendor. You'll then be able
-   ;; to load them.
-   ;;
-   ;; For example, if you download yaml-mode.el to ~/.emacs.d/vendor,
-   ;; then you can add the following code to this file:
-   ;;
-   ;; (require 'yaml-mode)
-   ;; (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-   ;; 
-   ;; Adding this code will make Emacs enter yaml mode whenever you open
-   ;; a .yml file
-   (add-to-list 'load-path "~/.emacs.d/vendor")
-
-
-;;;;
-   ;; Customization
-;;;;
-
-   ;; Add a directory to our load path so that when you `load` things
-   ;; below, Emacs knows where to look for the corresponding file.
-   (add-to-list 'load-path "~/.emacs.d/customizations")
-
-   ;; Sets up exec-path-from-shell so that Emacs will use the correct
-   ;; environment variables
-   (load "shell-integration.el")
-
-   ;; These customizations make it easier for you to navigate files,
-   ;; switch buffers, and choose options from the minibuffer.
-   (load "navigation.el")
-
-   ;; These customizations change the way emacs looks and disable/enable
-   ;; some user interface elements
-   (load "ui.el")
-
-   ;; These customizations make editing a bit nicer.
-   (load "editing.el")
-
-   ;; Hard-to-categorize customizations
-   (load "misc.el")
-
-   ;; For editing lisps
-   (load "elisp-editing.el")
-
-   ;; Langauage-specific
-   (load "setup-clojure.el")
-   (load "setup-slime.el")
-   (load "setup-js.el"))
-  )
-
-(load "ui.el")
