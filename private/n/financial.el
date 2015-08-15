21294cfde8ce38177cd7243f270e92e8f27e1d8c
南山 <junjiemars@gmail.com>
2015-08-15 13:33:27 +0800

    rename '/f to /. seems more intuitive

--- private/n/financial.el
+++ private/n/financial.el
@@ -1,6 +1,6 @@
 ;; Financial Calculations
 
-(defun /f (DIVIDEND &rest DIVISORS)
+(defun /. (DIVIDEND &rest DIVISORS)
   "Returns first float-point DIVIDEND divided by all the remaining DIVISORS"
   (-reduce '/ (cons (+ DIVIDEND 0.0) DIVISORS)))
 
@@ -27,30 +27,30 @@
 (defun present-value (F R T)
   "Returns the present value using simple interest. F is the future value, R is 
   the interest rate, and T is the times"
-  (/f F (+ 1 (* R T))))
+  (/. F (+ 1 (* R T))))
 
 (defun present-value+ (F R T)
   "Returns the present value using compound interest. F is the future value, R is
   the interest rate, and T is the times"
-  (/f F (expt (+ 1 R) T)))
+  (/. F (expt (+ 1 R) T)))
 
 (defun times (F P R)
   "Returns the times using simple interest. F is the future value, P is the 
   principal, and R is the interest rate"
-  (/f (- (/f F P) 1) R))
+  (/. (- (/. F P) 1) R))
 
 (defun times+ (F P R)
   "Returns the times using compound interest. F is the future value, P is the
   principal, and R is the interest rate"
-  (/f (log (/f F P)) (log (+ 1 R))))
+  (/. (log (/. F P)) (log (+ 1 R))))
 
 (defun rate (F P T)
   "Returns the simple interest rate. F is the future value, P is the principal,
   and T is times"
-  (/f (- (/f F P) 1) T))
+  (/. (- (/. F P) 1) T))
 
 (defun rate+ (F P T)
   "Returns the compound interest rate. F is the future value, P is the principal,
   and T is times"
-  (- (expt (/f F P) (/f 1 T)) 1))
+  (- (expt (/. F P) (/. 1 T)) 1))
 
