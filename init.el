c5e22c95c9f0870178afb2765900a131ff011367
南山 <junjiemars@gmail.com>
2015-08-30 20:54:58 +0800

    add `time' macro for calculating the elapsed time that s-expression took

--- init.el
+++ init.el
@@ -2,6 +2,16 @@
   "Ignores body, yields nil"
   nil)
 
+(defmacro time (expr)
+  "Evaluates expr and prints the time it took. Returns the value of
+  expr."
+  `(let ((start (current-time))
+         (return ,expr))
+     (print (format "Elapsed %f secs."
+                    (float-time (time-subtract (current-time)
+                                               start))))
+     return))
+
 (defconst loading-start-time
   (current-time) "The start time at loading init.el")
 
