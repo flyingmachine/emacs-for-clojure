9ae459d703e1343ceeb250e41b25abc3ef9e4afc
南山竹 <junjiemars@gmail.com>
2016-07-18 10:35:38 +0800

    let hl-line :background automaticly

--- config/editing.el
+++ config/editing.el
@@ -20,8 +20,9 @@
 
 ;; Current line highlight and background #3e4446
 (global-hl-line-mode 1)
-(unless (display-graphic-p)
-  (set-face-background 'hl-line "#393939"))
+(comment
+ (unless (display-graphic-p)
+   (set-face-background 'hl-line "#393939")))
 
 ;; Interactive search key bindings. By default, C-s runs
 ;; isearch-forward, so this swaps the bindings.
