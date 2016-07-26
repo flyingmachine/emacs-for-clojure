f7df3fc53c664054b6c9257a0e0ac3ef4e980a2b
南山竹 <junjiemars@gmail.com>
2016-07-26 23:40:26 +0800

    refine finacial

--- private/n/financial.el
+++ private/n/financial.el
@@ -4,70 +4,69 @@
   "Returns first float-point DIVIDEND divided by all the remaining DIVISORS"
   (cl-reduce '/ (cons (+ DIVIDEND 0.0) DIVISORS)))
 
-(defun rate/365 (R &optional annual?)
-  "Returns daily rate of R when the &optional annual? is true, others returns R"
-  (if annual? (/. R 365) R))
+(defun rate. (R &optional periods)
+  "Returns the rate of R on the spedified periods."
+  (let ((rt (if periods periods 1)))
+    (/. R rt)))
 
-(defun rate*365 (R &optional annual?)
-  "Returns annual rate of R when the &optional annual? is ture, others returns R"
-  (if annual? (* R 365) R))
-
-(defun interest (P R T &optional annual?)
-  "Return the earned simple interest, P is the pricipal, R is the 
-  interest rate, and T is times"
-  (let ((R1 (rate/365 R annual?)))
+(defun interest (P R T &optional periods)
+  "Return the earned simple interest, P is the principal, R is the 
+  interest rate, and T is the times"
+  (let ((R1 (rate. R periods)))
     (* P R1 T)))
 
-(defun future-value (P R T &optional annual?)
+(defun future-value (P R T &optional periods)
   "Returns the future value using simple interest. P is the principal, R is the
   interest rate, and T is the times"
-  (let ((R1 (rate/365 R annual?)))
+  (let ((R1 (rate. R periods)))
     (* P (+ 1 (* R1 T)))))
 
-(defun future-value+ (P R T &optional annual?)
+(defun future-value+ (P R T &optional periods)
   "Returns the future value using compound interest. P is the principal, R is the
   interest rate, and T is the times"
-  (let ((R1 (rate/365 R annual?)))
+  (let ((R1 (rate. R periods)))
     (* P (expt (+ 1 R1) T))))
 
-(defun interest+ (P R T &optional annual?)
+(defun interest+ (P R T &optional periods)
   "Return the earned compound interest, P is the pricipal, R is the
   interest rate, and T is times"
-  (let ((R1 (rate/365 R annual?)))
+  (let ((R1 (rate. R periods)))
     (- (future-value+ P R1 T) P)))
 
-(defun present-value (F R T &optional annual?)
+(defun present-value (F R T &optional periods)
   "Returns the present value using simple interest. F is the future value, R is 
   the interest rate, and T is the times"
-  (let ((R1 (rate/365 R annual?)))
+  (let ((R1 (rate. R periods)))
     (/. F (+ 1 (* R1 T)))))
 
-(defun present-value+ (F R T &optional annual?)
+(defun present-value+ (F R T &optional periods)
   "Returns the present value using compound interest. F is the future value, R is
   the interest rate, and T is the times"
-  (let ((R1 (rate/365 R annual?)))
+  (let ((R1 (rate. R periods)))
     (/. F (expt (+ 1 R1) T))))
 
-(defun times (F P R &optional annual?)
+(defun times (F P R &optional periods)
   "Returns the times using simple interest. F is the future value, P is the 
   principal, and R is the interest rate"
-  (let ((R1 (rate/365 R annual?)))
+  (let ((R1 (rate. R periods)))
     (/. (- (/. F P) 1) R1)))
 
-(defun times+ (F P R &optional annual?)
+(defun times+ (F P R &optional periods)
   "Returns the times using compound interest. F is the future value, P is the
   principal, and R is the interest rate"
-  (let ((R1 (rate/365 R annual?)))
+  (let ((R1 (rate. R periods)))
     (/. (log (/. F P)) (log (+ 1 R1)))))
 
-(defun rate (F P T &optional annual?)
+(defun rate (F P T &optional periods)
   "Returns the simple interest rate. F is the future value, P is the principal,
   and T is times"
-  (let ((R (/. (- (/. F P) 1) T)))
-    (rate*365 R annual?)))
+  (let ((R (/. (- (/. F P) 1) T))
+        (rt (if periods periods 1)))
+    (* R rt)))
 
-(defun rate+ (F P T &optional annual?)
+(defun rate+ (F P T &optional periods)
   "Returns the compound interest rate. F is the future value, P is the principal,
   and T is times"
-  (let ((R  (- (expt (/. F P) (/. 1 T)) 1)))
-    (rate*365 R annual?)))
+  (let ((R  (- (expt (/. F P) (/. 1 T)) 1))
+        (rt (if periods periods 1)))
+    (* R rt)))
