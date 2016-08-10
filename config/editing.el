84ddd55a6da407bc6c96b35c24f677cc364adfce
南山竹 <junjiemars@gmail.com>
2016-08-10 23:46:01 +0800

    hl-line from #393939 to #3e4446

--- config/editing.el
+++ config/editing.el
@@ -21,7 +21,7 @@
 ;; Current line highlight and background #3e4446
 (global-hl-line-mode 1)
 (unless (display-graphic-p)
-  (set-face-background 'hl-line "#393939"))
+  (set-face-background 'hl-line "#3e4446"))
 
 ;; Interactive search key bindings. By default, C-s runs
 ;; isearch-forward, so this swaps the bindings.
