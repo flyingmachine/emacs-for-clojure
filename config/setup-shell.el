3518653c9384983c24f5fe2787630e9a19f4d6b6
南山竹 <junjiemars@gmail.com>
2016-10-25 10:41:22 +0800

    clean setup-shell for windows

--- config/setup-shell.el
+++ config/setup-shell.el
@@ -11,26 +11,22 @@
 (platform-supported-p
  'darwin
  (exec-path-from-shell-initialize))
-  
+
+
 (platform-supported-p
    'gnu/linux
    (unless (getenv "SHELL")
-     (setenv "SHELL" "/bin/bash"))
-   (exec-path-from-shell-initialize))
+     (setenv "SHELL" "/bin/bash")
+     (exec-path-from-shell-initialize)))
 
 
 (platform-supported-p
  'windows-nt
- (let* ((win-bash "C:/Program Files/Git/usr/bin/bash.exe")
-        (win-bash-path (file-name-directory win-bash)))
-   ;; no better solution for bash on Windows
-   (when (file-exists-p win-bash)
-     (unless (file-exists-p "~/.emacs_bash")
-       (copy-file "~/.emacs.d/config/.emacs_bash"
-                  "~/.emacs_bash"))
-     (setenv "PATH" (concat win-bash-path (getenv "PATH")))
-     (setq shell-file-name "bash")
-     (setenv "SHELL" shell-file-name)
-     (safe-set! explicit-shell-file-name shell-file-name)
-     (add-hook
-      'comint-output-filter-function 'comint-strip-ctrl-m))))
+ (when (bin-exists-p "bash")
+   (unless (file-exists-p "~/.emacs_bash")
+        (copy-file "~/.emacs.d/config/.emacs_bash"
+                   "~/.emacs_bash"))
+   (setq shell-file-name "bash")
+   (setenv "SHELL" shell-file-name)
+   (safe-set! explicit-shell-file-name shell-file-name)))
+
