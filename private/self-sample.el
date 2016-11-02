225408abcaf0deaab8f35b7be48d012089cf6955
南山竹 <junjiemars@gmail.com>
2016-11-02 17:44:07 +0800

    now, support customized configurations, see private/self-sample.el

---
+++ private/self-sample.el
@@ -0,0 +1,13 @@
+;;;;
+;; self-sample.el: specified yourself private configuration elisp file
+;;                 and named it with self.el
+;;;;
+
+
+
+(comment
+ (platform-supported-p
+  gnu/linux
+  (defconst private-linux-font "White Rabbit-12"
+    "default font-size for gnu/linux")
+  ))
