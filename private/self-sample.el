fbd37ad9f8820d6813d61f81f3dcbcb9dea7bf8e
南山竹 <junjiemars@gmail.com>
2016-11-06 18:24:22 +0800

    defvar does not evalute the value part if the symbol's value cell is void, may be in trouble if upgrade self.el.

--- private/self-sample.el
+++ private/self-sample.el
@@ -8,26 +8,26 @@
 (comment
  (platform-supported-p
   gnu/linux
-  (defvar self-gnu/linux-font "White Rabbit-12"
+  (defconst self-gnu/linux-font "White Rabbit-12"
     "default font-size for gnu/linux")
-  (defvar self-gnu/linux-cjk-font (cons "Microsoft Yahei" 12)
+  (defconst self-gnu/linux-cjk-font (cons "Microsoft Yahei" 12)
     "default cjk font for gnu/linux")))
 
 (comment
  (platform-supported-p
   darwin
-  (defvar self-darwin-font "Monaco-13"
+  (defconst self-darwin-font "Monaco-13"
     "default font-size for darwin")
-  (defvar self-darwin-theme 'tomorrow-night-eighties
+  (defconst self-darwin-theme 'tomorrow-night-eighties
     "default theme for darwin")
-  (defvar self-darwin-packages '(geiser clojure lfe)
+  (defconst self-darwin-packages '(geiser clojure lfe)
     "default packages for darwin")
-  (defvar self-darwin-post-do 
+  (defconst self-darwin-post-do 
     (lambda () (message "#Self post-do ...")))))
 
 (comment
  (platform-supported-p
   windows-nt
-  (defvar self-windows-nt-font "Consolas-13"
+  (defconst self-windows-nt-font "Consolas-13"
     "default font-size for windows nt")
-  (defvar self-windows-nt-cjk-font (cons "Microsoft Yahei" 12))))
+  (defconst self-windows-nt-cjk-font (cons "Microsoft Yahei" 12))))
