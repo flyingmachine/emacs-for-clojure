625124c5a44025dd80247f8172229a0079bc856b
南山竹 <junjiemars@gmail.com>
2017-02-04 22:24:39 +0800

    clean

--- config/setup-sbcl.el
+++ config/setup-sbcl.el
@@ -18,19 +18,6 @@
  (safe-setq-inferior-lisp-program (bin-path "sbcl") t))
 
 
-(comment
- ;; Stop SLIME's REPL from grabbing DEL,
- ;; which is annoying when backspacing over a '('
- (defun override-slime-repl-bindings-with-paredit ()
-   (enable-paredit-mode)
-   (aggressive-indent-mode)
-   (rainbow-delimiters-mode)
-   (when (and (boundp 'slime-repl-mode-map)
-              (boundp 'paredit-backward-delete-key))
-     (define-key slime-repl-mode-map
-       (read-kbd-macro paredit-backward-delete-key) nil))))
-
-
 ;; setup sbcl, it's slow process so be adviced
 (defadvice slime (before slime-before compile)
   (add-hook
@@ -41,9 +28,7 @@
                              'slime-close-all-parens-in-sexp))
      (safe-do slime-selector 
               (global-set-key (kbd "C-c s")
-                              'slime-selector))
-     (comment
-      (override-slime-repl-bindings-with-paredit))))
+                              'slime-selector))))
   (slime-setup '(slime-fancy slime-asdf)))
 
 
