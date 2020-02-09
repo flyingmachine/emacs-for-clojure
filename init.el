4e65b1b9245ccc36afec4d9046d1e1607e11f00c
David Vujic <david@vujic.se>
2020-02-09 08:04:42 +0100

    fix: remove deprecated package archives

--- init.el
+++ init.el
@@ -5,9 +5,7 @@
 ;; Define package repositories
 (require 'package)
 (add-to-list 'package-archives
-             '("tromey" . "http://tromey.com/elpa/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+             '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
